4db25dba5465058f0ca4899a5244bbc19899d521
bapata <bapata@ymail.com>
2015-06-06 10:55:15 -0700

    Create palindrome.rb

---
+++ palindrome.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+# Script to check if a given word is a palindrome
+# Usage: ./palindrome.rb <word>
+# Example: ./palindrome.rb bob # output: bob is a palindrome
+#          ./palindrome.rb bob # output: bobo is not a palindrome
+
+# Main starts here
+word,= ARGV
+puts "#{word} is " + (word == word.reverse() ? "a palindrome" : "not a palindrome")
