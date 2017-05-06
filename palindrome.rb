81aefee4cef29c98693dd05357782a136a545427
bapata <bapata@ymail.com>
2017-05-06 15:01:42 -0700

    doc fixes

--- palindrome.rb
+++ palindrome.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
 
 # Script to check if a given word is a palindrome
-# Usage: ./palindrome.rb <word>
+# Usage: ./palindrome.rb <word-to-test>
 # Example: ./palindrome.rb bob # output: bob is a palindrome
-#          ./palindrome.rb bob # output: bobo is not a palindrome
+#          ./palindrome.rb bobo # output: bobo is not a palindrome
 
 # Main starts here
 word,= ARGV
