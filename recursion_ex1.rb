cccad4a0a3df730bb6768aa9d52287be51853a06
bapata <bapata@ymail.com>
2017-05-06 15:06:55 -0700

    Added prod_all

--- recursion_ex1.rb
+++ recursion_ex1.rb
@@ -10,7 +10,7 @@ def factorial(n)
 end
 
 
-# sum_upto(3) = 1+2+3 
+# sum_upto(3) = 1+2+3
 def sum_upto(n)
    if(n==1)
     return 1
@@ -31,10 +31,24 @@ def add_all(list)
   end
 end
 
+# Return product of all elements in a list
+# prod_all([1,2]) #=>2
+
+def prod_all(list)
+  if list.length() == 1
+    return list[0]
+  else
+    return list[0] * prod_all(list[1..list.length()-1])
+  end
+end
+
 sum = sum_upto(10)
 puts "Sum of all numbers upto 10 is #{sum}"
 product = factorial(10)
 puts "Factorial of 10 is #{product}"
-mylist=[1,2,3]
+mylist=[1,2,3,4]
 sum = add_all(mylist)
 puts "Sum of all numbers in #{mylist} is #{sum}"
+prod = prod_all(mylist)
+puts "Product of all numbers in #{mylist} is #{prod}"
+exit(0)
