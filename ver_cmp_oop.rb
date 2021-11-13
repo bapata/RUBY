b1169e84f5b8356cc7a9c18168422292b498a922
bapata <bapata@ymail.com>
2021-11-13 00:18:12 -0800

    Update ver_cmp_oop.rb

--- ver_cmp_oop.rb
+++ ver_cmp_oop.rb
@@ -1,24 +1,9 @@
 #!/usr/bin/env ruby
-
-# Object oriented implementation
 class VersionStr
-  public
-    # Initialize the name
-    def initialize(v)
-        @ver_str=v
-    end
-    def <=>(obj)
-        return ver_cmp(self.getver(),obj.getver())
-    end
-    def getver()
-      return @ver_str
-    end
-
 
   private
-    attr_accessor:ver_str
-     
-    # Private method for comparing 2 string versions
+    attr_reader :verstr
+
     def ver_cmp(v1,v2)
       v_intlist1=v1.split(".").map { |x| x.to_i() }
       v_intlist2=v2.split(".").map { |x| x.to_i() }
@@ -49,6 +34,19 @@ class VersionStr
       end
         return 9999
     end # ver_cmp
+
+
+  public
+    # Initialize the name
+    def initialize(v)
+        @verstr=v
+    end
+    def <=>(obj)
+        return ver_cmp(self.getver(),obj.getver())
+    end
+    def getver()
+      return verstr
+    end
 end # Class
 
 
